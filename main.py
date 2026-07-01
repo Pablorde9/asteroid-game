c8b97a4b16d76a2e094c546e8588ce224c3785eb
Pablorde <rcapablo7@gmail.com>
2026-07-01 11:04:25 -0300

    feat: instantiate player and screen rendering

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -8,13 +9,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
+    p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            dt = clock.tick(60) / 1000
         screen.fill("black")
+        p.draw(screen)
         pygame.display.flip()
 
 
